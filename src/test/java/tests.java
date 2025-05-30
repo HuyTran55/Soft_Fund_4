8320d389c82fb6c4ad09bdaefa23c94c237ffde8
HuyTran55 <s4096525@student.rmit.edu.au>
2025-05-30 11:35:48 +1000

    junit

---
+++ src/test/java/tests.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class tests {
+    @Test
+    void testTest() {
+        assertEquals(3, 1 + 1);
+    }
+}
